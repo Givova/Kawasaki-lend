d0058bc65d426805d2fd586a06c98cc460fd5abc
Kukushenok <gusarov.ml@mail.ru>
2025-06-19 12:13:32 +0300

    Адаптив и реквизиты

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -95,6 +95,17 @@ const ContactInfo = styled.div`
   }
 `;
 
+const InnLink = styled.a`
+  color: #ccc;
+  transition: color 0.3s ease;
+  cursor: pointer;
+  text-decoration: none;
+  
+  &:hover {
+    color: var(--primary-color);
+  }
+`;
+
 const SocialLinks = styled.div`
   display: flex;
   gap: 15px;
@@ -199,12 +210,19 @@ const Footer: React.FC = () => {
               </svg>
               <a href="mailto:info@kawasaki.ru">info@protechmoto.ru</a>
             </ContactInfo>
-            {/* <ContactInfo>
-              <svg viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12,2a8,8,0,0,0-8,8c0,5.4,7.05,11.5,7.35,11.76a1,1,0,0,0,1.3,0C13,21.5,20,15.4,20,10A8,8,0,0,0,12,2Zm0,17.65c-2.13-2-6-6.31-6-9.65a6,6,0,0,1,12,0C18,13.34,14.13,17.66,12,19.65ZM12,6a4,4,0,1,0,4,4A4,4,0,0,0,12,6Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,12,12Z" />
+            <ContactInfo>
+              <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14,2H6A2,2,0,0,0,4,4V20A2,2,0,0,0,6,22H18A2,2,0,0,0,20,20V8L14,2ZM18,20H6V4H13V9H18V20Z" />
               </svg>
-              <span>214012, г. Смоленск, Ново-Московская улица, 2/8</span>
-            </ContactInfo> */}
+              <InnLink 
+                href="/images/Реквизиты Новые решения_Точка.pdf" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                ООО "Новые решения"<br />
+                ИНН 6732243595
+              </InnLink>
+            </ContactInfo>
           </FooterColumn>
         </FooterContent>
 
